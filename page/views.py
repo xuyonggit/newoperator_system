8612ae6d32a498ad4871f9c3d95b245d576be517
lxg-gintong <lixuguang@gintong.com>
2018-12-18 13:35:21 +0800

    新界面

--- page/views.py
+++ page/views.py
@@ -1,29 +1,36 @@
 # -*- coding: utf-8 -*-
-import json
-
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from user.myFunctions import needLogin
 from page.models import url_name
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 def index(request):
-    data = '头号玩家'
-    return render(request, 'index_test.html', context={'data': data})
+    # data = '头号玩家'
+    data = url_name.objects.filter()
+    # print(data.values())
+    for i in data.values():
+        print(i)
+    return render(request, 'index_test.html', context={'data': data.values()})
 
 
 @csrf_exempt
 def names(request):
     if request.method == 'POST':
-
         uname = request.POST.get('uname')
         links = request.POST.get('links')
-        url_name.objects.create(
-            username=uname,
-            urls=links
-        )
-        return HttpResponse(json.dumps({"state": 0}))
+        userdata = url_name.objects.filter(username=uname)
+        print(len(userdata.values()))
+        if len(userdata.values()) == 0:
+            url_name.objects.create(
+                username=uname,
+                urls=links
+            )
+            return HttpResponse(json.dumps({"state": 0}))
+
+        else:
+            return HttpResponse(json.dumps({"state": 1}))
     else:
         return render(request, 'index_test.html')
-
